3691f75bf6886391be282843388e22731932a00b
ohrisBraun <ohrisbraun@gmail.com>
2020-06-26 23:14:43 +0300

    add paint

--- src/components/wall-insulation/WallInsulation.jsx
+++ src/components/wall-insulation/WallInsulation.jsx
@@ -1,11 +1,16 @@
+import w from './WallInsulation.module.css'
 import React, {useState} from 'react';
-import w from './WallInsulation.module.css';
+
 
 const WallInsulation = (props) => {
     let [height, setHeight] = useState("");
     let [width, setWidth] = useState("");
     let [wallMode, setWallMode] = useState(false)
+    let [wallColor, setwallColor] = useState("aliceblue");
+    let [total,setTotal]=useState(0);
+    let [paint, setPaint] = useState({color:"", price:0, paintTotal:false});
     let createWall = () => {
+        setTotal(Math.ceil((height * width / 1000000 * 4) * 100) / 100);
         setWallMode(true)
     }
     let heightRef = React.createRef();
@@ -16,35 +21,48 @@ const WallInsulation = (props) => {
     let heightChange = () => {
         setHeight(heightRef.current.value)
     }
+    let colorChange = (color,price) => {
+        setTotal(Math.ceil((height * width / 1000000 * 4+height * width / 1000000 * price) * 100) / 100)
+        setwallColor(color);
+        let paintBuffer = {color:color,price:price,paintTotal:true}
+        setPaint(paintBuffer);
 
+    }
 
     return (
-        <section className={`container ${w.wrapper}`}>
+        <div className={w.sectionOne}>
             {wallMode ?
-                <div className={`row`}>
-                    <div className={`col-6 ${w.Wall}`}>
-                        <div >Width {width} mm</div>
+                <div className={w.Section}>
+                    <div className={w.Wall} style={{background:wallColor}}>
+                        <div>Width {width} mm</div>
                         <div>Height {height} mm</div>
                     </div>
-                    <div className={`col-6 d-flex flex-column align-items-center justify-content-around`}>
-                        <div>
-                            You need {Math.ceil((height * width / 1000000) * 100) / 100} m2
-                        </div>
-                        <div>
-                            1 m2 price: 4$
-                        </div>
-                        <div>
-                            Total: {Math.ceil((height * width / 1000000 * 4) * 100) / 100}$
+                    <div className={w.TotalDiv}>
+                        <h2>Paint Color</h2>
+                        <div className={w.paintSection}>
+                            <div className={w.paintColor} onClick={()=>{colorChange("Red",5)}} title={"5$"} style={{background: "red"}}/>
+                            <div className={w.paintColor} onClick={()=>{colorChange("Green",10)}}  title={"10$"} style={{background: "green"}}/>
+                            <div className={w.paintColor} onClick={()=>{colorChange("Yellow",20)}}  title={"20$"} style={{background: "yellow"}}/>
+                            <div className={w.paintColor} onClick={()=>{colorChange("Blue",30)}}  title={"30$"} style={{background: "blue"}}/>
                         </div>
                     </div>
+                    <div className={w.TotalDiv}>
+                        Your wall {Math.ceil((height * width / 1000000) * 100) / 100} m2<br/>
+                        Insulation price: 4$ <br/>
+                        {paint.paintTotal?
+                            <div>
+                                {paint.color} paint price {paint.price}$
+                            </div>:<div></div>}
+                        Total: {total}$ <br/>
+                    </div>
+
                 </div>
                 :
-                <div className='row'>
-                    <h2 className={`col-lg-12 ${w.heading}`}>Insulation</h2>
-                    <div className={`col-lg-12 d-flex flex-column  align-items-center ${w.content}`}>
+                <div className={w.InsulationForm}>
+                    <h2>Insulation</h2>
+
                         <div>
-                            Width <input type="number" required={true} value={width} onChange={widthChange}
-                                         ref={widthRef}/> mm
+                            Width <input type="number" value={width} onChange={widthChange} ref={widthRef}/> mm
                         </div>
                         <br/>
                         <div>
@@ -52,15 +70,11 @@ const WallInsulation = (props) => {
                         </div>
                         <br/>
                         <div>
-                            <button type="button" className={`btn btn-primary ${w.btnAddWall}`}
-                                    onClick={createWall}>+
-                            </button>
-                            <span className={`${w.btnLabel}`}>Add wall</span>
-                        </div>
+                    <span>Add wall  </span>
+                        <button className={"btn btn-primary"} onClick={createWall}>+</button></div>
 
-                    </div>
                 </div>}
-        </section>
+        </div>
     );
 };
 
